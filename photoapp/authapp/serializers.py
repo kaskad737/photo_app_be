c9a6570a90d727d2523b44e13b38e2393ee93189
Apti <aptia@thinkeasy.cz>
2024-10-11 16:19:09 +0200

    add modules for cors to requirements, modify settings and serializer for login view and for frontend

--- photoapp/authapp/serializers.py
+++ photoapp/authapp/serializers.py
@@ -22,15 +22,15 @@ class RegisterSerializer(serializers.ModelSerializer):
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
 
-    password = serializers.CharField(write_only=True, required=True,
-                                     validators=[validate_password])
+    password1 = serializers.CharField(write_only=True, required=True,
+                                      validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
         fields = [
             'username',
-            'password',
+            'password1',
             'password2',
             'email',
             'first_name',
@@ -42,7 +42,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         }
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
+        if attrs['password1'] != attrs['password2']:
             raise serializers.ValidationError({
                 'password': 'Password fields didn\'t match.'
             })
@@ -56,7 +56,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name']
         )
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data['password1'])
         user.save()
 
         return user
--- photoapp/photoapp/settings/base.py
+++ photoapp/photoapp/settings/base.py
@@ -28,6 +28,10 @@ DEBUG = bool(strtobool(os.getenv("DEBUG", "False")))
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+]
+
 
 # Application definition
 
@@ -39,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'drf_spectacular',
     'rest_framework_simplejwt',
     'authapp.apps.AuthappConfig',
@@ -50,6 +55,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
