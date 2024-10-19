b7b4ba7b323656c84e34853857ac0a90dae47c34
Apti <aptia@thinkeasy.cz>
2024-10-19 22:28:23 +0200

    modify photo model add frame model

--- photoapp/imageapp/models.py
+++ photoapp/imageapp/models.py
@@ -1,9 +1,21 @@
 from django.db import models
+import uuid
 
 
 class Photo(models.Model):
-    photo = models.ImageField(upload_to='photos/')  # Фотография сохраняется в директорию 'photos'
-    upload_time = models.DateTimeField(auto_now_add=True)  # Время загрузки
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    photo = models.ImageField(upload_to='photos/')
+    qrcode = models.ImageField(upload_to='qrcode/')
+    upload_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.photo.name
+
+
+class Frame(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    frame = models.ImageField(upload_to='frames/')
+    upload_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.frame.name
