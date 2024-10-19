7fc2462c881b0591f8628781235b4b8f10c6e5f8
Apti <aptia@thinkeasy.cz>
2024-10-19 22:28:35 +0200

    add frame serializer

--- photoapp/imageapp/serializers.py
+++ photoapp/imageapp/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Photo
+from .models import Photo, Frame
 
 
 class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
         fields = ['id', 'photo']
+
+
+class FrameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Frame
+        fields = ['id', 'frame']
