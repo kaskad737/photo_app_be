190b84803d675d33b4e7afe3b57562727655ac1c
Apti <aptia@thinkeasy.cz>
2024-10-19 22:26:56 +0200

    add custom user model

--- photoapp/authapp/models.py
+++ photoapp/authapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+
+class UserRole(models.TextChoices):
+    PHOTOGRAPHER = 'photographer', 'Photographer'
+    ADMIN = 'admin', 'Admin'
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    role = models.CharField(max_length=15, choices=UserRole.choices, default=UserRole.PHOTOGRAPHER)
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
