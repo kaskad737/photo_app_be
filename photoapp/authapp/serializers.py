d740a7c6311ec53b86b2265a29a8d7c6c7e8462f
Apti <aptia@thinkeasy.cz>
2024-10-19 22:27:27 +0200

    change to custom user model

--- photoapp/authapp/serializers.py
+++ photoapp/authapp/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-from django.contrib.auth.models import User
+from authapp.models import User
 from django.contrib.auth.password_validation import validate_password
 
 
