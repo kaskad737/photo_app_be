5f06269d8943639b4bf4997e294528590c994e59
Apti <aptia@thinkeasy.cz>
2024-10-19 22:28:52 +0200

    add urls for frames

--- photoapp/imageapp/urls.py
+++ photoapp/imageapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import UploadPhotoView, DownloadPhotoView
+from .views import UploadPhotoView, DownloadPhotoView, UploadFrameView, ListFramesView
 
 app_name = 'imageapp'
 
 urlpatterns = [
-    path('upload/', UploadPhotoView.as_view(), name='upload-photo'),  # Эндпоинт для загрузки фотографии
-    path('download/<int:pk>/', DownloadPhotoView.as_view(), name='download-photo'),  # Эндпоинт для скачивания
+    path('upload/', UploadPhotoView.as_view(), name='upload-photo'),
+    path('download/<uuid:pk>/', DownloadPhotoView.as_view(), name='download-photo'),
+    path('upload_frame/', UploadFrameView.as_view(), name='download-frame'),
+    path('list_frames/', ListFramesView.as_view(), name='list-frames')
 ]
