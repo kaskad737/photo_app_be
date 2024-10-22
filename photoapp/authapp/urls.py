6d68e58c483558a646bf3248c1518d19852d6fbc
Apti <aptia@thinkeasy.cz>
2024-10-22 20:34:50 +0200

    add urls for user actions

--- photoapp/authapp/urls.py
+++ photoapp/authapp/urls.py
@@ -3,18 +3,24 @@ from .views import (
     CustomTokenObtainPairView,
     CustomTokenRefreshView,
     CustomTokenVerifyView,
-    UserRegisterView,
+    UserInvitationView,
     UsersListView,
     UserRetrieveUpdateDestroyView,
+    UserCreatePasswordView,
+    UserCancelInvitationView,
+    UserDeactivateView,
 )
 
 app_name = 'authapp'
 
 urlpatterns = [
-    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', CustomTokenVerifyView.as_view(), name='token_verify'),
-    path('user_register/', UserRegisterView.as_view(), name='user_register'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token-refresh'),
+    path('token/verify/', CustomTokenVerifyView.as_view(), name='token-verify'),
+    path('user_invite/', UserInvitationView.as_view(), name='user-invite'),
+    path('user_password_create/', UserCreatePasswordView.as_view(), name='user-password-create'),
+    path('user_cancel_invitation/', UserCancelInvitationView.as_view(), name='user-cancel-invitation'),
+    path('user_deactivate/', UserDeactivateView.as_view(), name='user-deactivate'),
     path('users/', UsersListView.as_view(), name='users'),
-    path('users/<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user_details'),
+    path('users/<uuid:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user-details'),
 ]
