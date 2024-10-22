0b6e42838d0acd5474b7f978e03c8e6af6f616af
Apti <aptia@thinkeasy.cz>
2024-10-22 20:33:56 +0200

    add additional columns to user model

--- photoapp/authapp/models.py
+++ photoapp/authapp/models.py
@@ -11,6 +11,9 @@ class UserRole(models.TextChoices):
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     role = models.CharField(max_length=15, choices=UserRole.choices, default=UserRole.PHOTOGRAPHER)
+    is_invited = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    password_changed_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.username} ({self.get_role_display()})"
