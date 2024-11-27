a6fce75335ddd5ce086f60d4f0bcb93ae240a926
Apti <aptia@thinkeasy.cz>
2024-11-27 21:09:40 +0100

    add urls for generating report and restaurant

--- photoapp/workdayapp/urls.py
+++ photoapp/workdayapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import ShiftStartAPIView, ShiftEndAPIView
+from .views import ShiftStartAPIView, ShiftEndAPIView, ExcelReportView, RestaurantCreateView, RestaurantListView
 
 app_name = 'workdayapp'
 
 urlpatterns = [
     path('shift/start/', ShiftStartAPIView.as_view(), name='shift-start'),
     path('shift/end/', ShiftEndAPIView.as_view(), name='shift-end'),
+    path("generate-report/", ExcelReportView.as_view(), name="generate-report"),
+    path('restaurants/', RestaurantListView.as_view(), name='restaurant-list'),
+    path('restaurants/register/', RestaurantCreateView.as_view(), name='restaurant-register'),
 ]
