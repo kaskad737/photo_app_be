a34fa7ee39dcbafb289f1f9a3881cdd56040f622
Apti <aptia@thinkeasy.cz>
2024-11-27 21:09:19 +0100

    add uuid for restaurant

--- photoapp/workdayapp/models.py
+++ photoapp/workdayapp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from authapp.models import User
+import uuid
 
 
 class Restaurant(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
