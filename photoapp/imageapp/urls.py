8ac5e51e8d1eada7ceb1f055a56761b8a3b644a1
Apti <aptia@thinkeasy.cz>
2024-11-30 22:34:39 +0100

    add urls for list and retrieve photos and frames

--- photoapp/imageapp/urls.py
+++ photoapp/imageapp/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import UploadPhotoView, DownloadPhotoView, UploadFrameView, ListFramesView
+from .views import (
+    UploadPhotoView,
+    DownloadPhotoView,
+    UploadFrameView,
+    ListFramesView,
+    ListPhotosView,
+    RetrievePhotoView,
+)
 
 app_name = 'imageapp'
 
@@ -7,5 +14,7 @@ urlpatterns = [
     path('upload/', UploadPhotoView.as_view(), name='upload-photo'),
     path('download/<uuid:pk>/', DownloadPhotoView.as_view(), name='download-photo'),
     path('upload_frame/', UploadFrameView.as_view(), name='download-frame'),
-    path('list_frames/', ListFramesView.as_view(), name='list-frames')
+    path('list_frames/', ListFramesView.as_view(), name='list-frames'),
+    path('list_photos/', ListPhotosView().as_view(), name='list-photos'),
+    path('photo/<uuid:pk>/', RetrievePhotoView().as_view(), name='photo'),
 ]
